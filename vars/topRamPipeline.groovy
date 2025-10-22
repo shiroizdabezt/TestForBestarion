53edc56050852085adadcfa56abc026aae03e1da
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-10-22 23:43:02 +0700

    b

--- vars/topRamPipeline.groovy
+++ vars/topRamPipeline.groovy
@@ -1,14 +1,14 @@
 def call(Map cfg = [:]) {
-  String label = cfg.label ?: 'pc'
-  String path  = cfg.scriptPath ?: '/home/jenkins/top_ram.sh'
+  def nodeLabel  = (cfg.label ?: 'pc')
+  def scriptPath = (cfg.scriptPath ?: '/home/jenkins/top_ram.sh')
 
   pipeline {
-    agent { label label }
+    agent { label "${nodeLabel}" }
     stages {
       stage('Show RAM Usage') {
         steps {
-          sh "chmod +x '${path}' || true"
-          sh "'${path}'"
+          sh "chmod +x '${scriptPath}' || true"
+          sh "'${scriptPath}'"
         }
       }
     }
