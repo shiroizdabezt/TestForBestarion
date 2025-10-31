b21badaf2e1850bb541b4bebc4e402fd0b3990f0
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-10-31 08:47:58 +0700

    test: clone cpsScm scm

---
+++ jenkins_home/casc/jobs/jcasc_listner.groovy
@@ -0,0 +1,26 @@
+pipelineJob('casc-pipeline-2') {
+  description('JCasC managed pipeline for testing')
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url("${GITHUB_BRANCH}")
+            credentials('github-key')
+          }
+          branch('*/**')
+        }
+      }
+      scriptPath('jenkins_home/casc/Jenkinsfile')
+    }
+  }
+
+  triggers {
+    githubPush()
+  }
+
+  properties {
+    disableConcurrentBuilds()
+  }
+}
