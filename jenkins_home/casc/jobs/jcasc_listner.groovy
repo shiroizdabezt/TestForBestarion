66db30d9d7e2daa0e931b7bcd8c69acc666dd78c
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-11-04 11:32:50 +0700

    test

--- jenkins_home/casc/jobs/jcasc_listner.groovy
+++ jenkins_home/casc/jobs/jcasc_listner.groovy
@@ -1,43 +1,42 @@
-pipelineJob('casc-pipeline-4') {
-  description('JCasC managed pipeline for testin')
+pipeline {
+  agent any
 
-  definition {
-    cps {
-      script('''
-        node {
-          properties([
-            pipelineTriggers([
-              [
-                \$class: 'GenericTrigger',
-                genericVariables: [
-                  [key: 'ref',    value: '\\$.ref',     expressionType: 'JSONPath', regexpFilter: '', defaultValue: ''],
-                  [key: 'before', value: '\\$.before',  expressionType: 'JSONPath', regexpFilter: '', defaultValue: '']
-                ],
-                genericRequestVariables: [
-                  [key: 'requestWithNumber', regexpFilter: '[^0-9]'],
-                  [key: 'requestWithString', regexpFilter: '']
-                ],
-                genericHeaderVariables: [
-                  [key: 'headerWithNumber', regexpFilter: '[^0-9]'],
-                  [key: 'headerWithString', regexpFilter: '']
-                ],
-                causeString: 'Triggered on \$ref',
-                token: 'abc123',
-                tokenCredentialId: '',
-                printContributedVariables: true,
-                printPostContent: true,
-                silentResponse: false,
-                shouldNotFlatten: false,
-                regexpFilterText: '\$ref',
-                regexpFilterExpression: '.*'
-              ]
-            ])
-          ])
+  triggers {
+    genericTrigger(
+      genericVariables: [
+        [key: 'ref',    value: '$.ref',     expressionType: 'JSONPath', regexpFilter: '', defaultValue: ''],
+        [key: 'before', value: '$.before',  expressionType: 'JSONPath', regexpFilter: '', defaultValue: '']
+      ],
+      genericRequestVariables: [
+        [key: 'requestWithNumber', regexpFilter: '[^0-9]'],
+        [key: 'requestWithString', regexpFilter: '']
+      ],
+      genericHeaderVariables: [
+        [key: 'headerWithNumber', regexpFilter: '[^0-9]'],
+        [key: 'headerWithString', regexpFilter: '']
+      ],
 
-          echo "✅ Webhook triggered for ref: \$ref"
-        }
-      '''.stripIndent())
-      sandbox(true)
+      causeString: 'Triggered on $ref',
+
+      token: 'abc123',
+      tokenCredentialId: '',
+
+      printContributedVariables: true,
+      printPostContent: true,
+      silentResponse: false,
+      shouldNotFlatten: false,
+
+      // chỉ nhận webhook khi ref khớp nhánh hiện tại
+      regexpFilterText: '$ref',
+      regexpFilterExpression: '.*'  // cho phép tất cả các nhánh
+    )
+  }
+
+  stages {
+    stage('Webhook received') {
+      steps {
+        echo "✅ Webhook triggered successfully on ref: ${ref}"
+      }
     }
   }
 }
