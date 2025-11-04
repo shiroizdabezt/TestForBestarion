0d1bb04d0be6f69fa2cd4545ef5c907b0553a3f6
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-11-04 11:46:28 +0700

    test

--- jenkins_home/casc/jobs/jcasc_listner.groovy
+++ jenkins_home/casc/jobs/jcasc_listner.groovy
@@ -1,12 +1,35 @@
 pipelineJob('jcasc-listener') {
   description('Pipeline lắng nghe Generic Webhook (tạo bằng Job DSL)')
-
   definition {
     cps {
       script('''\
 pipeline {
   agent any
+
   stages {
+    stage('init props (once)') {
+      steps {
+        script {
+          properties([
+            pipelineTriggers([
+              [$class: 'GenericTrigger',
+                genericVariables: [[key: 'ref', value: '\\$.ref']],
+                causeString: 'Triggered on $ref',
+                token: 'abc123',
+                tokenCredentialId: '',
+                printContributedVariables: true,
+                printPostContent: true,
+                silentResponse: false,
+                shouldNotFlatten: false,
+                regexpFilterText: '$ref',
+                regexpFilterExpression: '.*'
+              ]
+            ])
+          ])
+        }
+      }
+    }
+
     stage('Some step') {
       steps {
         sh 'echo "$ref"'
@@ -17,23 +40,5 @@ pipeline {
 ''')
       sandbox(true)
     }
-  }
-
-  // ✅ đây là chỗ làm cho Generic Webhook Trigger được tick trong UI
-  properties {
-    pipelineTriggers([
-      [$class: 'GenericTrigger',
-        genericVariables: [[key: 'ref', value: '\\$.ref']],
-        causeString: 'Triggered on $ref',
-        token: 'abc123',
-        tokenCredentialId: '',
-        printContributedVariables: true,
-        printPostContent: true,
-        silentResponse: false,
-        shouldNotFlatten: false,
-        regexpFilterText: '$ref',
-        regexpFilterExpression: '.*'
-      ]
-    ])
   }
 }
