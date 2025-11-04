c1dc34418ffd48cb08557290b127aa9d6ecb9c85
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-11-04 14:55:23 +0700

    test

--- jenkins_home/casc/jobs/test.groovy
+++ jenkins_home/casc/jobs/test.groovy
@@ -14,7 +14,7 @@ pipeline {
         sh 'echo "User: $user_name"'
         sh 'echo "Ref: $ref"'
         sh 'echo "Commit: $commit"'
-        sh 'echo "main2"'
+        sh 'echo "main3"'
       }
     }
   }
