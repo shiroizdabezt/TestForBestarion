f356f35282c504e29c947004a4ae705585e29f95
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-11-04 11:38:10 +0700

    test

--- jenkins_home/casc/jobs/jcasc_listner.groovy
+++ jenkins_home/casc/jobs/jcasc_listner.groovy
@@ -4,16 +4,7 @@ pipeline {
   triggers {
     genericTrigger(
       genericVariables: [
-        [key: 'ref',    value: '$.ref',     expressionType: 'JSONPath', regexpFilter: '', defaultValue: ''],
-        [key: 'before', value: '$.before',  expressionType: 'JSONPath', regexpFilter: '', defaultValue: '']
-      ],
-      genericRequestVariables: [
-        [key: 'requestWithNumber', regexpFilter: '[^0-9]'],
-        [key: 'requestWithString', regexpFilter: '']
-      ],
-      genericHeaderVariables: [
-        [key: 'headerWithNumber', regexpFilter: '[^0-9]'],
-        [key: 'headerWithString', regexpFilter: '']
+        [key: 'ref', value: '$.ref']
       ],
 
       causeString: 'Triggered on $ref',
@@ -23,19 +14,19 @@ pipeline {
 
       printContributedVariables: true,
       printPostContent: true,
+
       silentResponse: false,
       shouldNotFlatten: false,
 
-      // chỉ nhận webhook khi ref khớp nhánh hiện tại
       regexpFilterText: '$ref',
-      regexpFilterExpression: '.*'  // cho phép tất cả các nhánh
+      regexpFilterExpression: ".*"
     )
   }
 
   stages {
-    stage('Webhook received') {
+    stage('Some step') {
       steps {
-        echo "✅ Webhook triggered successfully on ref: ${ref}"
+        sh 'echo "$ref"'
       }
     }
   }
