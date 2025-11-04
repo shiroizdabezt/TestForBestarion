e16d04fe7c166ed82935411aad1c5c441903f99f
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-11-04 11:03:05 +0700

    test

--- jenkins_home/casc/jobs/jcasc_listner.groovy
+++ jenkins_home/casc/jobs/jcasc_listner.groovy
@@ -1,4 +1,4 @@
-pipelineJob('casc-pipeline-5') {
+pipelineJob('casc-pipeline-4') {
   description('JCasC managed pipeline for testin')
 
   definition {
@@ -17,22 +17,27 @@ pipelineJob('casc-pipeline-5') {
     }
   }
 
-  properties {
-    disableConcurrentBuilds()
-    pipelineTriggers([
-    [$class: 'GenericTrigger',
+  triggers {
+    genericTrigger(
       genericVariables: [
         [key: 'ADDED',    value: '$.commits[*].added[*]',    expressionType: 'JSONPath'],
         [key: 'MODIFIED', value: '$.commits[*].modified[*]', expressionType: 'JSONPath'],
         [key: 'REMOVED',  value: '$.commits[*].removed[*]',  expressionType: 'JSONPath'],
-        [key: 'REF',      value: '$.ref',                     expressionType: 'JSONPath']
+        [key: 'REF',      value: '$.ref',                     expressionType: 'JSONPath'] // refs/heads/main
       ],
-      token: 'abc',
-      printContributedVariables: true,
-      printPostContent: false,
+      token: 'abc',   // dùng trong URL webhook
+      printContributedVariables: true,        // bật log để debug
+      printPostContent: false,                // bật true nếu muốn xem full payload (cân nhắc log)
+      // Chuỗi dữ liệu đem đi lọc regex
       regexpFilterText: '$ADDED,$MODIFIED,$REMOVED|$REF',
+      // Chỉ TRIGGER nếu có file trong thư mục cần theo dõi (vd jenkins_home/casc/)
+      // + tuỳ chọn: chỉ trên nhánh main
+      // Giải thích: phần trước dấu | là danh sách file; sau | là ref
       regexpFilterExpression: '(?s).*(?:^|,)(?:jenkins_home/casc/)[^,]+.*\\|refs/heads/main'
-    ]
-  ])
+    )
+  }
+
+  properties {
+    disableConcurrentBuilds()
   }
 }
