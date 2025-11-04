8ebf4316dafba4f6e0832c4bce9694bbef8dfd63
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-11-04 11:47:51 +0700

    test

--- jenkins_home/casc/jobs/jcasc_listner.groovy
+++ jenkins_home/casc/jobs/jcasc_listner.groovy
@@ -1,35 +1,12 @@
 pipelineJob('jcasc-listener') {
   description('Pipeline lắng nghe Generic Webhook (tạo bằng Job DSL)')
+
   definition {
     cps {
       script('''\
 pipeline {
   agent any
-
   stages {
-    stage('init props (once)') {
-      steps {
-        script {
-          properties([
-            pipelineTriggers([
-              [$class: 'GenericTrigger',
-                genericVariables: [[key: 'ref', value: '\\$.ref']],
-                causeString: 'Triggered on $ref',
-                token: 'abc123',
-                tokenCredentialId: '',
-                printContributedVariables: true,
-                printPostContent: true,
-                silentResponse: false,
-                shouldNotFlatten: false,
-                regexpFilterText: '$ref',
-                regexpFilterExpression: '.*'
-              ]
-            ])
-          ])
-        }
-      }
-    }
-
     stage('Some step') {
       steps {
         sh 'echo "$ref"'
@@ -39,6 +16,33 @@ pipeline {
 }
 ''')
       sandbox(true)
+    }
+  }
+
+  // Thêm job property "PipelineTriggersJobProperty" chứa GenericTrigger
+  configure { node ->
+    def props = node / 'properties'
+    def triggersProp = props / 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty'
+    def triggers = triggersProp / 'triggers'
+    triggers / 'org.jenkinsci.plugins.gwt.GenericTrigger' {
+      causeString('Triggered on $ref')
+      token('abc123')
+      tokenCredentialId('')
+      printContributedVariables(true)
+      printPostContent(true)
+      silentResponse(false)
+      shouldNotFlatten(false)
+      regexpFilterText('$ref')
+      regexpFilterExpression('.*')
+      genericVariables {
+        'org.jenkinsci.plugins.gwt.GenericVariable' {
+          key('ref')
+          value('\\$.ref')      // quan trọng: escape $
+          expressionType('JSONPath')
+          defaultValue('')
+          regexpFilter('')
+        }
+      }
     }
   }
 }
