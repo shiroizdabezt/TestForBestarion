b18b0f6c28e14a4badcfe1d2c0e111aad96ac8ab
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-11-04 11:24:05 +0700

    test

--- jenkins_home/casc/jobs/jcasc_listner.groovy
+++ jenkins_home/casc/jobs/jcasc_listner.groovy
@@ -2,51 +2,42 @@ pipelineJob('casc-pipeline-4') {
   description('JCasC managed pipeline for testin')
 
   definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            url("https://github.com/shiroizdabezt/TestForBestarion.git")
-            credentials('github-key')
-          }
-          branch('*/**')
+    cps {
+      script("""
+        node {
+          properties([
+            pipelineTriggers([
+              [
+                \$class: 'GenericTrigger',
+                genericVariables: [
+                  [key: 'ref',    value: '\\$.ref',     expressionType: 'JSONPath', regexpFilter: '', defaultValue: ''],
+                  [key: 'before', value: '\\$.before',  expressionType: 'JSONPath', regexpFilter: '', defaultValue: '']
+                ],
+                genericRequestVariables: [
+                  [key: 'requestWithNumber', regexpFilter: '[^0-9]'],
+                  [key: 'requestWithString', regexpFilter: '']
+                ],
+                genericHeaderVariables: [
+                  [key: 'headerWithNumber', regexpFilter: '[^0-9]'],
+                  [key: 'headerWithString', regexpFilter: '']
+                ],
+                causeString: 'Triggered on \$ref',
+                token: 'abc123',
+                tokenCredentialId: '',
+                printContributedVariables: true,
+                printPostContent: true,
+                silentResponse: false,
+                shouldNotFlatten: false,
+                regexpFilterText: '\$ref',
+                regexpFilterExpression: '.*'
+              ]
+            ])
+          ])
+
+          echo "✅ Webhook triggered for ref: \$ref"
         }
-      }
-      scriptPath('jenkins_home/casc/Jenkinsfile')
-      lightweight(true)
+      """.stripIndent())
+      sandbox(true)
     }
   }
-
-properties([
-    pipelineTriggers([
-      [$class: 'GenericTrigger',
-        genericVariables: [
-          [key: 'ref',    value: '\$.ref',     expressionType: 'JSONPath', regexpFilter: '', defaultValue: ''],
-          [key: 'before', value: '\$.before',  expressionType: 'JSONPath', regexpFilter: '', defaultValue: '']
-        ],
-        genericRequestVariables: [
-          [key: 'requestWithNumber', regexpFilter: '[^0-9]'],
-          [key: 'requestWithString', regexpFilter: '']
-        ],
-        genericHeaderVariables: [
-          [key: 'headerWithNumber', regexpFilter: '[^0-9]'],
-          [key: 'headerWithString', regexpFilter: '']
-        ],
-
-        causeString: 'Triggered on $ref',
-
-        token: 'abc123',
-        tokenCredentialId: '',
-
-        printContributedVariables: true,
-        printPostContent: true,
-        silentResponse: false,
-        shouldNotFlatten: false,
-
-        // chỉ nhận webhook khi ref khớp nhánh hiện tại
-        regexpFilterText: '$ref',
-        regexpFilterExpression: ".*"
-      ]
-    ])
-  ])
 }
