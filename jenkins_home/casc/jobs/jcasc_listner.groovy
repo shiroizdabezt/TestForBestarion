5f238af2e63a2f956a81d722f02a59430cb93c29
Shiroizdabezt <codykhoanguyen@gmail.com>
2025-11-04 11:07:32 +0700

    test

--- jenkins_home/casc/jobs/jcasc_listner.groovy
+++ jenkins_home/casc/jobs/jcasc_listner.groovy
@@ -17,25 +17,53 @@ pipelineJob('casc-pipeline-4') {
     }
   }
 
-  triggers {
-    genericTrigger {
-      genericVariables: [
-        [key: 'ADDED',    value: '$.commits[*].added[*]',    expressionType: 'JSONPath'],
-        [key: 'MODIFIED', value: '$.commits[*].modified[*]', expressionType: 'JSONPath'],
-        [key: 'REMOVED',  value: '$.commits[*].removed[*]',  expressionType: 'JSONPath'],
-        [key: 'REF',      value: '$.ref',                     expressionType: 'JSONPath'] // refs/heads/main
-      ],
-      token: 'abc',   // dùng trong URL webhook
-      printContributedVariables: true,        // bật log để debug
-      printPostContent: false,                // bật true nếu muốn xem full payload (cân nhắc log)
-      // Chuỗi dữ liệu đem đi lọc regex
-      regexpFilterText: '$ADDED,$MODIFIED,$REMOVED|$REF',
-      // Chỉ TRIGGER nếu có file trong thư mục cần theo dõi (vd jenkins_home/casc/)
-      // + tuỳ chọn: chỉ trên nhánh main
-      // Giải thích: phần trước dấu | là danh sách file; sau | là ref
-      regexpFilterExpression: '(?s).*(?:^|,)(?:jenkins_home/casc/)[^,]+.*\\|refs/heads/main'
-    )
+triggers {
+  genericTrigger {
+    genericVariables {
+      genericVariable {
+        key("ADDED")
+        value("\$.commits[*].added[*]")
+        expressionType("JSONPath")
+        regexpFilter("")
+        defaultValue("")
+      }
+      genericVariable {
+        key("MODIFIED")
+        value("\$.commits[*].modified[*]")
+        expressionType("JSONPath")
+        regexpFilter("")
+        defaultValue("")
+      }
+      genericVariable {
+        key("REMOVED")
+        value("\$.commits[*].removed[*]")
+        expressionType("JSONPath")
+        regexpFilter("")
+        defaultValue("")
+      }
+      genericVariable {
+        key("REF")
+        value("\$.ref")
+        expressionType("JSONPath")
+        regexpFilter("")
+        defaultValue("")
+      }
+    }
+    genericRequestVariables { }
+    genericHeaderVariables { }
+
+    token('abc')
+    tokenCredentialId('')
+    printContributedVariables(true)
+    printPostContent(false)
+    silentResponse(false)
+    shouldNotFlatten(false)
+
+    // Lọc chỉ khi có thay đổi trong jenkins_home/casc/ và nhánh main
+    regexpFilterText("\$ADDED,\$MODIFIED,\$REMOVED|\$REF")
+    regexpFilterExpression("(?s).*(?:^|,)(?:jenkins_home/casc/)[^,]+.*\\|refs/heads/main")
   }
+}
 
   properties {
     disableConcurrentBuilds()
